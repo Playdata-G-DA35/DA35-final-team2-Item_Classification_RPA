d5978ec325fe19a0c361bd599462cb600f7f60a0
item <sangjun0322@gamil.com>
2024-08-11 08:54:05 +0900

    server

--- django_1/mypro/settings.py
+++ django_1/mypro/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-&!73ov)h&q^ttws&8-5%g2z3(qzph)b83^*9=g#(=*9q75f52s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
