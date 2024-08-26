27d53b295a37f1b4dee7080da9d57ada7de56de9
koseoyeong <alsk2902@naver.com>
2024-08-26 17:29:21 +0900

    합친거.......

---
+++ plus/market/admin.py
@@ -0,0 +1,12 @@
+# admin.py
+
+from django.contrib import admin
+from .models import User, Category, Product, ProductFile, Chat, UserProfile, ProductCheck, check2
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductFile)
+admin.site.register(Chat)
+admin.site.register(UserProfile)
+admin.site.register(ProductCheck)
+admin.site.register(check2)
