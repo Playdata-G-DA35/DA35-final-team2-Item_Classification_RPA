325e4c3e3453a11cbf9a967abaa3eca3c0dd68d9
koseoyeong <alsk2902@naver.com>
2024-08-30 18:26:02 +0900

    Final

---
+++ Django/market/admin.py
@@ -0,0 +1,14 @@
+# admin.py
+
+from django.contrib import admin
+from .models import User, Category, Product, ProductFile, Chat, UserProfile, ProductCheck, check2, FinalModel,  FindImage
+
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(ProductFile)
+admin.site.register(Chat)
+admin.site.register(UserProfile)
+admin.site.register(ProductCheck)
+admin.site.register(check2)
+admin.site.register(FinalModel)
+admin.site.register(FindImage)
